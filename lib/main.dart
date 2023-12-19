807132d668ec99799ed5b1ac834ea1266e8155ae
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-12-19 08:42:52 +0500

    add router

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-
-import 'presentation/pages/splash_screen_page.dart';
+import 'presentation/routes/app_router.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'My app', // used by the OS task switcher
-      home: SafeArea(
-        child: SplashScreenPage(),
-      ),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'My App',
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: '/',
+    );
+  }
 }
---
+++ lib/presentation/routes/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_application/presentation/pages/home_page.dart';
+import '../pages/splash_screen_page.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const SplashScreenPage());
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
