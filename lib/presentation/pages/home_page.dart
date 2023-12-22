38c04bafa0738dfbcfb0cf92379da709e66d4d54
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-12-22 12:21:11 +0500

    update home_page.dart

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_application/presentation/widgets/product.dart';
 import 'package:ecommerce_application/presentation/widgets/tag.dart';
 import 'package:flutter/material.dart';
 
@@ -34,72 +35,109 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: const EdgeInsets.all(28.0),
           child: Center(
-            child: Flexible(
-              child: Column(
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(
-                        Icons.menu,
-                        size: 30,
-                      ),
-                      Icon(
-                        Icons.shopping_basket_outlined,
-                        size: 30,
-                      ),
-                    ],
-                  ),
-                  const Padding(padding: EdgeInsets.all(12)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const SizedBox(
-                        width: 300,
-                        height: 50,
-                        child: Center(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Seach for brand or product name',
-                            ),
-                          ),
-                        ),
-                      ),
-                      // SizedBox(
-                      const Padding(padding: EdgeInsets.all(5)),
-                      Expanded(
-                        child: SizedBox(
-                          height: 50,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.grey)),
-                            child: const Icon(Icons.filter_alt_outlined),
-                          ),
-                        ),
-                      ),
-
-                      // ),
-                    ],
-                  ),
-                  const Padding(padding: EdgeInsets.all(20)),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: shoeBrands
-                          .map(
-                            (brand) => [
-                              Tag(brand),
-                              const SizedBox(width: 8),
-                            ],
-                          )
-                          .expand((element) => element)
-                          .take(shoeBrands.length * 2 - 1)
-                          .toList(),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      size: 30,
                     ),
-                  )
-                ],
-              ),
+                    Icon(
+                      Icons.shopping_basket_outlined,
+                      size: 30,
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.all(12)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      width: 300,
+                      height: 50,
+                      child: Center(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Seach for brand or product name',
+                          ),
+                        ),
+                      ),
+                    ),
+                    // SizedBox(
+                    const Padding(padding: EdgeInsets.all(5)),
+                    Expanded(
+                      child: SizedBox(
+                        height: 50,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey)),
+                          child: const Icon(Icons.filter_alt_outlined),
+                        ),
+                      ),
+                    ),
+
+                    // ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.all(20)),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: shoeBrands
+                        .map(
+                          (brand) => [
+                            Tag(brand),
+                            const SizedBox(width: 8),
+                          ],
+                        )
+                        .expand((element) => element)
+                        .take(shoeBrands.length * 2 - 1)
+                        .toList(),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.all(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "New Arrival",
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print("See All");
+                      },
+                      child: const Text("See all",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          )),
+                    ),
+                  ],
+                ),
+                const Padding(padding: EdgeInsets.all(20)),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    children: const [
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                      Product("AFI Crater flyknit nn (gs)", 670),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
